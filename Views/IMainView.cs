b5e08626228a92e8e268706726d75f716f314376
Chinonso Agbo <agbochinonso7@gmail.com>
2024-09-22 20:54:46 +0200

    Added remaining files

---
+++ Views/IMainView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudWinFormMVP.Views
+{
+    internal interface IMainView
+    {
+        event EventHandler ShowPetView;
+       // event EventHandler ShowPetView;
+    }
+}
